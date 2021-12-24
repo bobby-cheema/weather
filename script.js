5c46a100a008d5319efd18a06cf93a66e7112cdc
bobby cheema <bobbycheema@bobbys-MacBook-Pro.local>
2021-12-25 09:12:11 +1100

    added fuctionality and fixed code

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const inputEL = $("#input-city");
 const searchbtnEL = $("#search-btn");
 const btn1EL = $("#btn1");
 const btn2EL = $("#btn2");
+
+const btn3EL = $("#btn3");
+const btn4EL = $("#btn4");
+const btn5EL = $("#btn5");
+
+
+
+
+
 const cityEL = $("#displaycity");
 const dateEL = $("#displaydate");
 const iconEL = $("#displayicon");
@@ -15,8 +24,9 @@ const uvEL = $("#displayUV");
 searchbtnEL.on("click", (e) => {
     e.preventDefault();
     console.log("button pressed");
-    const mydata = inputEL.val();
-    console.log(mydata);
+    const searchfor = inputEL.val();
+    console.log(searchfor);
+    filldata(searchfor)
 });
 const getdata = async() => {
     console.log("step1 ");
@@ -36,7 +46,8 @@ const get5days = async(long, lat) => {
     if (long && lat) {
         console.log(`getting data for ${long}  ${lat}`);
         const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${long}&lon=${lat}&exclude=current,minutely,hourly,alerts&units=metric&appid=${API_key}`, { cache: "force-cache" }
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${long}&lon=${lat}&exclude=current,minutely,hourly,alerts&units=metric&appid=${API_key}`,
+
         );
         if (response.status === 200) {
             const data = await response.json();
@@ -52,7 +63,7 @@ const getcoords = async(city) => {
         console.log(`found ${city} in localstorage will send ${localdata}`);
         return localdata;
     } else {
-        console.log("Getting from network")
+        console.log("Getting from network");
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2f3f656925a35903ae2c9a034877c0b1`, { cache: "force-cache" }
         );
@@ -85,8 +96,8 @@ const getcoords = async(city) => {
 // };
 let cardEL = "";
 const startEL = $("#Start");
-const createcard = (mtdt, temp, wind_speed, humidity) => {
 
+const createcard = (mtdt, temp, wind_speed, humidity) => {
     cardEL = `<div class="col flex-wrap">
     <div class="card mycard ">
         <div   class="card-body =">
@@ -126,10 +137,11 @@ const write2cards = (data) => {
 };
 
 const getonce = async(lon, lat, city) => {
+    console.log("RUNNING GETONCE ......");
     if (lat && lon) {
         console.log(`getting data for ${lon}  ${lat}`);
         const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${lon}&lon=${lat}&exclude=daily,minutely,hourly,alerts&units=metric&appid=${API_key}`, { cache: "force-cache" }
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${lon}&lon=${lat}&exclude=daily,minutely,hourly,alerts&units=metric&appid=${API_key}`, {}
         );
         if (response.status === 200) {
             const data = await response.json();
@@ -141,73 +153,85 @@ const getonce = async(lon, lat, city) => {
 };
 
 btn1EL.on("click", async(e) => {
+    filldata("Sydney");
+});
+btn2EL.on("click", async(e) => {
+    filldata("Melbourne");
+});
+btn3EL.on("click", async(e) => {
+    filldata("Adelaide");
+});
+btn4EL.on("click", async(e) => {
+    filldata("Brisbane");
+});
+btn5EL.on("click", async(e) => {
+    filldata("Perth");
+});
+// btn2EL.on("click", async(e) => {
+//     const { lat, lon } = await getcoords("Melbourne");
 
-    const { lat, lon } = await getcoords("Sydney");
+//     const days5 = await get5days(lat, lon);
+//     const singledata = await getonce(lat, lon, "Melbourne");
+//     console.log("GIOT SINGLE DATA", singledata)
+//     if (singledata) {
+//         const temprature = await singledata.current.temp;
+//         const UVI = singledata.current.uvi;
+//         const windspeed = singledata.wind_speed;
+//         const humidity = singledata.current.humidity;
+//         const singledate = new Date(singledata.current.dt * 1000)
+//             .toLocaleString()
+//             .substring(1, 10);
+
+//         dateEL.html(`<h2 class="d-inline"> (${singledate})</h2>`);
+//         tempEL.html(`<h6 class="mt-3"> Temp:${temprature} C </h6>`);
+//         windEL.html(`<h6> Wind:${singledata.current.wind_speed} Kmph </h6>`);
+//         humidityEL.html(`<h6> Humidity:${singledata.current.humidity} % </h6>`);
+//         singledata.current.uvi < 3 ?
+//             (uvclass = "bg-success") :
+//             (uvclass = "bg-danger");
+//         uvEL.html(
+//             `<h6 class="${uvclass} text-light" > UV index:${singledata.current.uvi} </h6>`
+//         );
+//     } else {
+//         console.log("Failed to get SINGLEDATA line 150 ")
+//     }
+
+//     write2cards(days5.daily);
+// });
+
+const filldata = async(city) => {
+    const { lat, lon } = await getcoords(city);
     console.log(lat, lon);
     const days5 = await get5days(lat, lon);
     const singledata = await getonce(lat, lon);
-    if (singledata) {
-        const temprature = await singledata.current.temp;
-        const UVI = singledata.current.uvi;
-        const windspeed = singledata.wind_speed;
-        const humidity = singledata.current.humidity;
-        const singledate = new Date(singledata.current.dt * 1000)
-            .toLocaleString()
-            .substring(1, 10);
+    console.log("Got data from getonce func ", singledata)
+    const temprature = await singledata.current.temp;
+    const UVI = singledata.current.uvi;
+    const windspeed = singledata.wind_speed;
+    const humidity = singledata.current.humidity;
+    const singledate = new Date(singledata.current.dt * 1000)
+        .toLocaleString()
+        .substring(1, 10);
 
-        dateEL.html(`<h2 class="d-inline"> (${singledate})</h2>`);
-        tempEL.html(`<h6 class="mt-3"> Temp:${temprature} C </h6>`);
-        windEL.html(`<h6> Wind:${singledata.current.wind_speed} Kmph </h6>`);
-        humidityEL.html(`<h6> Humidity:${singledata.current.humidity} % </h6>`);
-        singledata.current.uvi < 3 ?
-            (uvclass = "bg-success") :
-            (uvclass = "bg-danger");
-        uvEL.html(
-            `<h6 class="${uvclass} text-light" > UV index:${singledata.current.uvi} </h6>`
-        );
-    }
-
-    write2cards(days5.daily);
-});
-
-btn2EL.on("click", async(e) => {
-    const { lat, lon } = await getcoords("Melbourne");
-
-    const days5 = await get5days(lat, lon);
-    const singledata = await getonce(lat, lon, "Melbourne");
-    if (singledata) {
-        const temprature = await singledata.current.temp;
-        const UVI = singledata.current.uvi;
-        const windspeed = singledata.wind_speed;
-        const humidity = singledata.current.humidity;
-        const singledate = new Date(singledata.current.dt * 1000)
-            .toLocaleString()
-            .substring(1, 10);
-        cityEL.html(`<h2 class="d-inline">Melbourne </h6>`)
-        dateEL.html(`<h2 class="d-inline"> (${singledate})</h2>`);
-        tempEL.html(`<h6 class="mt-3"> Temp:${temprature} C </h6>`);
-        windEL.html(`<h6> Wind:${singledata.current.wind_speed} Kmph </h6>`);
-        humidityEL.html(`<h6> Humidity:${singledata.current.humidity} % </h6>`);
-        singledata.current.uvi < 3 ?
-            (uvclass = "bg-success") :
-            (uvclass = "bg-danger");
-        uvEL.html(
-            `<h6 class="${uvclass} text-light" > UV index:${singledata.current.uvi} </h6>`
-        );
-    }
+    dateEL.html(`<h2 class="d-inline"> (${singledate})</h2>`);
+    console.log(" dateEL", dateEL)
+    cityEL.html(`<h2 class="d-inline">${city}</h6>`);
+    tempEL.html(`<h6 class="mt-3"> Temp:${temprature} C </h6>`);
+    windEL.html(`<h6> Wind:${singledata.current.wind_speed} Kmph </h6>`);
+    humidityEL.html(`<h6> Humidity:${singledata.current.humidity} % </h6>`);
+    singledata.current.uvi < 3 ?
+        (uvclass = "bg-success") :
+        (uvclass = "bg-danger");
+    uvEL.html(
+        `<h6 class="${uvclass} text-light" > UV index:${singledata.current.uvi} </h6>`
+    );
 
     write2cards(days5.daily);
-});
-
-const filldata = async() => {
-
-
-
-    }
-    // fetch("https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2f3f656925a35903ae2c9a034877c0b1")
-    //     .then(response => response.json())
-    //     .then(data => console.log(data))
-    //     .catch(err => console.log(err))
+};
+// fetch("https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2f3f656925a35903ae2c9a034877c0b1")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
 
 // fetch("https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2f3f656925a35903ae2c9a034877c0b1")
 //     .then((response) => {
