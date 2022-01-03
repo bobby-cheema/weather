f108ce070261622e0209a4b39fb03d2b0950e03f
bobbby cheema <bobbby1cheema@gmail.com>
2022-01-03 13:56:04 +1100

    fixed date in for loop, removed un wanted consoles and comments

--- script.js
+++ script.js
@@ -19,28 +19,24 @@ const uvEL = $("#displayUV");
 
 searchbtnEL.on("click", (e) => {
     e.preventDefault();
-    console.log("button pressed");
+
     const searchfor = inputEL.val();
-    console.log(searchfor);
+
     filldata(searchfor);
 });
 const getdata = async() => {
-    console.log("step1 ");
     const response = await fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2f3f656925a35903ae2c9a034877c0b1"
     );
     if (response.status === 200) {
-        console.log("step 2");
         const data = await response.json();
 
-        console.log("step 3");
         return data;
     }
 };
 
 const get5days = async(long, lat) => {
     if (long && lat) {
-        console.log(`getting data for ${long}  ${lat}`);
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/onecall?lat=${long}&lon=${lat}&exclude=current,minutely,hourly,alerts&units=metric&appid=${API_key}`
         );
@@ -55,40 +51,25 @@ const get5days = async(long, lat) => {
 const getcoords = async(city) => {
     const localdata = JSON.parse(localStorage.getItem(city));
     if (localdata) {
-        console.log(`found ${city} in localstorage will send ${localdata}`);
         return localdata;
     } else {
-        console.log("Getting from network");
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2f3f656925a35903ae2c9a034877c0b1`, { cache: "force-cache" }
         );
         if (response.status === 200) {
             const data = await response.json();
             if (data) {
-                console.log(`typeof ${typeof data} valueof ${data} }  `);
-
                 for (const [key, value] of Object.entries(data)) {
-                    console.log(`key: ${key} value:${value}`);
                     if (key == "coord") {
                         localStorage.setItem(city, JSON.stringify(value));
                         return value;
                     }
-                    // if (typeof(value) === 'object') {
-                    //     console.log(`${key}  is an object`)
-                    //     console.log(Object.entries(value))
-
-                    // }
-                    // console.log(key, value)
                 }
             }
         }
     }
 };
-// write2dom = (data) => {
-//   if (data) {
-//     console.log("DOM", data);
-//   }
-// };
+
 let cardEL = "";
 const startEL = $("#Start");
 
@@ -119,14 +100,12 @@ const createcard = (mtdt, temp, wind_speed, humidity, cardicon) => {
     startEL.append(cardEL);
 };
 const write2cards = (data) => {
-    $(".cardclass").remove()
+    $(".cardclass").remove();
     if (data) {
-        console.log("write2cards", data);
-        for (let day = 0; day < 5; day++) {
+        for (let day = 1; day < 6; day++) {
             const mydt = new Date(data[day].dt * 1000).toDateString();
             const { temp, uvi, wind_speed, humidity, weather } = data[day];
-            const cardicon = weather[0].icon
-            console.log("UVI", uvi);
+            const cardicon = weather[0].icon;
 
             createcard(mydt, temp.day, wind_speed, humidity, cardicon);
         }
@@ -134,9 +113,7 @@ const write2cards = (data) => {
 };
 
 const getonce = async(lon, lat, city) => {
-    console.log("RUNNING GETONCE ......");
     if (lat && lon) {
-        console.log(`getting data for ${lon}  ${lat}`);
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/onecall?lat=${lon}&lon=${lat}&exclude=daily,minutely,hourly,alerts&units=metric&appid=${API_key}`, {}
         );
@@ -164,62 +141,31 @@ btn4EL.on("click", async(e) => {
 btn5EL.on("click", async(e) => {
     filldata("Perth");
 });
-// btn2EL.on("click", async(e) => {
-//     const { lat, lon } = await getcoords("Melbourne");
-
-//     const days5 = await get5days(lat, lon);
-//     const singledata = await getonce(lat, lon, "Melbourne");
-//     console.log("GIOT SINGLE DATA", singledata)
-//     if (singledata) {
-//         const temprature = await singledata.current.temp;
-//         const UVI = singledata.current.uvi;
-//         const windspeed = singledata.wind_speed;
-//         const humidity = singledata.current.humidity;
-//         const singledate = new Date(singledata.current.dt * 1000)
-//             .toLocaleString()
-//             .substring(1, 10);
-
-//         dateEL.html(`<h2 class="d-inline"> (${singledate})</h2>`);
-//         tempEL.html(`<h6 class="mt-3"> Temp:${temprature} C </h6>`);
-//         windEL.html(`<h6> Wind:${singledata.current.wind_speed} Kmph </h6>`);
-//         humidityEL.html(`<h6> Humidity:${singledata.current.humidity} % </h6>`);
-//         singledata.current.uvi < 3 ?
-//             (uvclass = "bg-success") :
-//             (uvclass = "bg-danger");
-//         uvEL.html(
-//             `<h6 class="${uvclass} text-light" > UV index:${singledata.current.uvi} </h6>`
-//         );
-//     } else {
-//         console.log("Failed to get SINGLEDATA line 150 ")
-//     }
-// `<img src="${mysrc}" alt="weather icon"  height="10px" width="10px"></img`
-
-
-//     write2cards(days5.daily);
-// });
 
 const filldata = async(city) => {
     const { lat, lon } = await getcoords(city);
-    console.log(lat, lon);
+
     const days5 = await get5days(lat, lon);
     const singledata = await getonce(lat, lon);
-    console.log("Got data from getonce func ", singledata);
+
     const temprature = await singledata.current.temp;
     const UVI = singledata.current.uvi;
     const windspeed = singledata.wind_speed;
     const humidity = singledata.current.humidity;
     const myicon = singledata.current.weather[0].icon;
-    console.log("icon", myicon);
+
     const singledate = new Date(singledata.current.dt * 1000)
         .toLocaleString()
-        .substring(1, 10);
+        .substring(0, 10);
 
     dateEL.html(`<h2 class="d-inline"> (${singledate})</h2>`);
-    console.log(" iconEL", iconEL);
+
     cityEL.html(`<h2 class="d-inline mb-0">${city}</h6>`);
     tempEL.html(`<h6 class="mt-3"> Temp:${temprature} C </h6>`);
     const mysrc = `http://openweathermap.org/img/wn/${myicon}@2x.png`;
-    iconEL.html(`<img src="${mysrc}" alt="weather icon"  height="50px" width="50px"></img`);
+    iconEL.html(
+        `<img src="${mysrc}" alt="weather icon"  height="50px" width="50px"></img`
+    );
     windEL.html(`<h6> Wind:${singledata.current.wind_speed} Kmph </h6>`);
     humidityEL.html(`<h6> Humidity:${singledata.current.humidity} % </h6>`);
     singledata.current.uvi < 3 ?
@@ -231,52 +177,3 @@ const filldata = async(city) => {
 
     write2cards(days5.daily);
 };
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2f3f656925a35903ae2c9a034877c0b1")
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
-
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=2f3f656925a35903ae2c9a034877c0b1")
-//     .then((response) => {
-//         if (response.status === 200) {
-//             return response.json()
-//         }
-//     })
-//     .then((data) => { console.log(data) })
-
-// const mydata = getdata()
-// console.log("step 4")
-// console.log(mydata)
-
-// (async() => {
-
-//     mydata = await getdata();
-//     //[base, clouds, , coordinates, , main, namee, ] = mydata
-//     // for (const [key, value] of(Object.entries(mydata))) {
-//     //     console.log(`${key}: ${value}`)
-//     // }
-//     // const [lat, lon] = getcoords()
-//     // console.log(`longtitute: ${lon} latitite:${lat}`)
-//     const { lat, lon } = getcoords()
-//     console.log(`longtitute: ${lon} latitite:${lat}`)
-
-// })()
-
-// let getcoords = () => {
-//     if (mydata) {
-//         for (const [key, value] of(Object.entries(mydata))) {
-//             if (key === "coord") {
-//                 return value
-//             }
-//             //console.log(`${key}: ${value}`)
-//         }
-
-//     } else {
-//         console.log("no mydata")
-//     }
-
-// }
-
-// const myarr = ["onething", "twothing", "something"]
-// const [one, two, three] = myarr
-// console.log(three)
